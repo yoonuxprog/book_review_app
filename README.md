c8665e2c83ac7cd12d26fb8c874c47ebb0310e32
yoonuxprog <57206892+yoonuxprog@users.noreply.github.com>
2020-05-13 01:59:36 +0530

    completed register

---
+++ README.md
@@ -0,0 +1,3 @@
+# Project 1: Books
+
+Web Programming with Python and JavaScript
---
+++ application.py
@@ -0,0 +1,37 @@
+import os
+
+from flask import Flask, session, render_template, request
+from flask_session import Session
+from sqlalchemy import create_engine
+from sqlalchemy.orm import scoped_session, sessionmaker
+
+app = Flask(__name__)
+
+# Check for environment variable
+if not os.getenv("DATABASE_URL"):
+    raise RuntimeError("DATABASE_URL is not set")
+
+# Configure session to use filesystem
+app.config["SESSION_PERMANENT"] = False
+app.config["SESSION_TYPE"] = "filesystem"
+Session(app)
+
+# Set up database
+engine = create_engine(os.getenv("DATABASE_URL"))
+db = scoped_session(sessionmaker(bind=engine))
+
+
+@app.route("/")
+def index():
+    return render_template("register.html")
+
+@app.route("/reg", methods = ["POST"])
+def reg():
+    #get form information
+    username = request.form.get("username")
+    password = request.form.get("password")
+
+    #insert information to database
+    db.execute("INSERT INTO users (username, password) VALUES (:username, :password)", {"username": username, "password": password})
+    db.commit()
+    return render_template("home.html")
